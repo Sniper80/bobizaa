c9fd604c51e4a21e51bb92cf9e3ba71092b87e13
Sniper80 <75849429+Sniper80@users.noreply.github.com>
2024-02-05 20:58:07 +0200

    Update config.js

--- config.js
+++ config.js
@@ -10,23 +10,23 @@ import moment from 'moment-timezone';
 global.botnumber = ""
 
 global.owner = [
-  ['212605784394', 'bobiza', true],
+  ['201020182886', '𝐒𝐍𝐈𝐏𝐄𝐑 ༄❤️', true],
 
-  ['212605784394','bobiza', true],
-  ['212605784394'],
-  ['212605784394']
+  ['201020182886','𝐒𝐍𝐈𝐏𝐄𝐑 ༄❤️', true],
+  ['201020182886'],
+  ['201020182886']
 ];
 
-global.suittag = ['212605784394'];
-global.prems = ['212605784394'];
+global.suittag = ['201020182886'];
+global.prems = ['201020182886'];
 
-global.packname = 'bobiza bot';
-global.author = 'bobiza';
-global.wm = 'bobiza';
-global.titulowm = 'bobiza';
-global.titulowm2 = `bobiza`
-global.igfg = 'bobiza';
-global.wait = '*|🏃| loading...*\n*نحاول تلبية طلبكم لا تستعجلوا* ♥\ninstagram.com/noureddine_ouafy';
+global.packname = '𝐒𝐍𝐈𝐏𝐄𝐑 ༄❤️';
+global.author = '01020182886';
+global.wm = '𝐒𝐍𝐈𝐏𝐄𝐑 ༄❤️';
+global.titulowm = '𝐒𝐍𝐈𝐏𝐄𝐑 ༄❤️';
+global.titulowm2 = `𝐒𝐍𝐈𝐏𝐄𝐑 ༄❤️`
+global.igfg = '𝐒𝐍𝐈𝐏𝐄𝐑 ༄❤️';
+global.wait = '*|🏃| loading...*\n*نحاول تلبية طلبكم لا تستعجلوا* ♥\ninstagram.com/s_.n._.i._.p._.e._.r';
 global.imagen1 = fs.readFileSync('./Menu2.jpg');
 global.imagen2 = fs.readFileSync('./src/nuevobot.jpg');
 global.imagen3 = fs.readFileSync('./src/Pre Bot Publi.png');
@@ -45,15 +45,15 @@ global.mes = d.toLocaleDateString('en', {month: 'long'});
 global.año = d.toLocaleDateString('en', {year: 'numeric'});
 global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
 //* ****************************
-global.wm2 = `${dia} ${fecha}\nBOBIZA`;
-global.gt = 'BOBIZA';
-global.mysticbot = 'The BOBIZA';
-global.md = 'https://instagram.com/noureddine_ouafy';
-global.mysticbot = 'https://instagram.com/noureddine_ouafy';
+global.wm2 = `${dia} ${fecha}\nSNIPER`;
+global.gt = 'SNIPER';
+global.mysticbot = 'SNIPER';
+global.md = 'https://instagram.com/s_.n._.i._.p._.e._.r';
+global.mysticbot = 'https://instagram.com/s_.n._.i._.p._.e._.r';
 global.waitt = '*[ ⏳ ] LOADING...*';
 global.waittt = '*[ ⏳ ] LOADING...*';
 global.waitttt = '*[ ⏳ ] LOADING...*';
-global.nomorown = '212605784394';
+global.nomorown = '201020182886';
 global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf'];
 global.cmenut = '❖––––––『';
 global.cmenub = '┊✦ ';
